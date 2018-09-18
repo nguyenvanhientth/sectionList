02e4ec1d15f448db44f1bf2b3ef229ddb16feb53
Nguyen Van Hien <nguyenvanhien200895@gmail.com>
2018-09-18 14:47:42 +0700

    first commit

---
+++ data/sectionlistData.js
@@ -0,0 +1,93 @@
+var sectionlistData = [
+    {
+        data:[
+            {
+                name: 'San 1',
+                andress: 'Ngo Van So'
+            },
+            {
+                name: 'San 2',
+                andress: 'Au co'
+            },
+            {
+                name: 'San 3',
+                andress: 'Lac Long Quan'
+            },
+            {
+                name: 'San 4',
+                andress: 'Pham Nhu Xuong'
+            },
+            {
+                name: 'San 5',
+                andress: 'Ninh Ton'
+            },
+            {
+                name: 'San 6',
+                andress: 'Thanh Vinh'
+            },
+        ],
+    
+    title: "Khu vuc Hoa Khanh"
+    },
+    {
+        data:[
+            {
+                name: 'San 11',
+                andress: 'Ngo Van So'
+            },
+            {
+                name: 'San 22',
+                andress: 'Au co'
+            },
+            {
+                name: 'San 33',
+                andress: 'Lac Long Quan'
+            },
+            {
+                name: 'San 44',
+                andress: 'Pham Nhu Xuong'
+            },
+            {
+                name: 'San 55',
+                andress: 'Ninh Ton'
+            },
+            {
+                name: 'San 66',
+                andress: 'Thanh Vinh'
+            },
+        ],
+    
+    title: "Khu vuc Hai Chau"
+    },
+    {
+        data:[
+            {
+                name: 'San a1',
+                andress: 'Ngo Van So'
+            },
+            {
+                name: 'San a2',
+                andress: 'Au co'
+            },
+            {
+                name: 'San a3',
+                andress: 'Lac Long Quan'
+            },
+            {
+                name: 'San a4',
+                andress: 'Pham Nhu Xuong'
+            },
+            {
+                name: 'San a5',
+                andress: 'Ninh Ton'
+            },
+            {
+                name: 'San a6',
+                andress: 'Thanh Vinh'
+            },
+        ],
+    
+    title: "Khu vuc Thanh Khe"
+    }
+];
+export {sectionlistData};
